c9f158b99c7f25bad646982961979ffc0da6cb8c
Sandeep Yadav <sandeepuim10@gmail.com>
2022-03-10 23:59:46 +0530

    All Page Portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Portfolio from "./components/Portfolio/Portfolio";
+import Resume from "./components/Resume/Resume";
+import Testimonials from "./components/Testimonials/Testimonials";
 
 const App = () => {
-  return (
-    <div>
-      <h1>Portfolio</h1>
-    </div>
-  )
-}
+  const [resumeData, setResumeData] = useState({});
 
-export default App
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Resume data={resumeData.resume} />
+      <Portfolio data={resumeData.portfolio} />
+      <Testimonials data={resumeData.testimonials} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;
